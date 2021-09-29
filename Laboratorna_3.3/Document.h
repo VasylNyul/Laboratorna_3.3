689c0b5890412828e7f60d7d20825bded204fc70
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2021-09-29 15:25:21 +0300

    Initial Commit

---
+++ Laboratorna_3.3/Document.h
@@ -0,0 +1,14 @@
+//
+//  Document.h
+//  Laboratorna_3.3
+//
+//  Created by apple on 29.09.2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+
+@end
+
---
+++ Laboratorna_3.3/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Laboratorna_3.3
+//
+//  Created by apple on 29.09.2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
